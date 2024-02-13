bad06bd2f573b0f23dc14813ef49aa78df17320f
Max Schaefer <maxexcloo@users.noreply.github.com>
2024-02-13 16:09:01 +1100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/expect
 set timeout -1
 spawn oxyromon {*}$argv
-sleep 2
-while {1} { expect -re { ".*Please select a ROM:.*" { sleep 0.2; send "\r" } } }
-wait
+while {1} { expect -re { ".*Please select a ROM:.*" { send "\r" } } }
